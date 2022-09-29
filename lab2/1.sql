ce4cec7f7d4d65c477f0882373941b84d5cac95d
qorganbek <dimashqorganbek@gmail.com>
2022-09-29 19:29:10 +0600

    lab3

--- lab2/1.sql
+++ lab2/1.sql
@@ -1,3 +1,7 @@
-CREATE TABLE PERSON(
-    
-)
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
