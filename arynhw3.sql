218bcd4d3181eacf459f408b07ed9bac9416e3d0
qorganbek <dimashqorganbek@gmail.com>
2022-10-01 19:05:06 +0600

    hw3

---
+++ arynhw3.sql
@@ -0,0 +1,39 @@
+
+
+create table employees(
+    emp_no INT primary key,
+    birth_date date,
+    first_name varchar(14),
+    last_name varchar(16),
+    gender gender,
+    hire_date date
+);
+create table departments(
+    dept_no char(4) primary key,
+    dept_name varchar(40)
+);
+create table dept_manager(
+    dept_no char(4) references departments(dept_no),
+    emp_no int references employees(emp_no),
+    from_date date,
+    to_date date
+);
+create table dept_emp(
+    emp_no int references employees(emp_no),
+    dept_no char(4) references departments(dept_no),
+    from_date date,
+    to_date date
+);
+create table salaries(
+    emp_no int references employees(emp_no),
+    salary int,
+    from_date date primary key ,
+    to_date date
+);
+create table titles(
+    emp_no int references employees(emp_no),
+    title varchar(50),
+    from_date date,
+    to_date date,
+    primary key (emp_no,title,from_date)
+);
--- hw3.sql
+++ hw3.sql
@@ -25,7 +25,7 @@
 
 --EX 2
 -- create database hw3;
-
+--
 -- create  type g as ENUM ('M','F');
 
 
@@ -48,9 +48,8 @@ create table if not exists departments(
 create table if not exists salaries(
     emp_no integer references employees(emp_no),
     salary integer,
-    from_date date,
-    to_date date,
-    primary key (emp_no,salary)
+    from_date date primary key ,
+    to_date date
 );
 
 create table if not exists title(
@@ -58,7 +57,16 @@ create table if not exists title(
     title varchar(50),
     from_date date,
     to_date date,
-    primary key (emp_no,title,from_date)
+    primary key (title,from_date)
+);
+
+
+create table if not exists dept_emp(
+    emp_no integer references employees(emp_no),
+    dept_no char(4) references departments(dept_no),
+    from_date date,
+    to_date date
+
 );
 
 create table if not exists dept_manager(
@@ -68,13 +76,77 @@ create table if not exists dept_manager(
     to_date date
 );
 
-create table if not exists dept_emp(
-    emp_no integer references employees(emp_no),
-    dept_no char(4) references departments(dept_no),
-    from_date date,
-    to_date date,
-    primary key (emp_no,dept_no)
+--ex3;
+create table if not exists students_table(
+    full_name varchar(100),
+    age integer,
+    birth_day date,
+    gender g,
+    avg_grade float,
+    nationality varchar(100),
+    phone_number varchar(100),
+    social_category varchar(50)
 );
+-- create type rm_lesson as ENUM('Yes','No')
+
+
+create table if not exists instructors_table(
+     full_name varchar(100),
+     speaking_languages varchar(255),
+     work_experience varchar(255),
+     pos_rm_lesson rm_lesson
+);
+
+create table if not exists student_relatives(
+    full_name varchar(100),
+    address varchar(100),
+    phone_number varchar(100),
+    position varchar(55)
+);
+-- create type honors as ENUM ('Yes','No');
+create table if not exists student_social_data(
+    school varchar(100),
+    graduation_data date,
+    addres varchar(100),
+    region varchar(100),
+    counrty varchar(100),
+    gpa float,
+    honors honors
+);
+
+
+--ex4
+--insert
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (9,'2003-12-19','Dinmukhammed','Korganbek','M','2022-10-01');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (2,'1961-04-16','Ibadat','Ablaeva','F','2022-10-02');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (3,'1983-01-07','Almas','Sailaubekov','M','2022-10-03');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (4,'1982-12-15','Nalikul','Omirbekova','F','2022-10-04');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (5,'2007-01-27','Nurdaulet','Ongarbek','M','2022-10-05');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (6,'2013-10-09','Erdaulet','Ongarbek','M','2022-10-01');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (7,'2005-06-15','Aliya','Duisenbek','F','2022-10-01');
+insert into employees(emp_no, birth_date, first_name, last_name, gender, hire_date)
+values (8,'1955-02-23','Ongarbek','Sailaubekov','M','2022-10-02');
+--insert
+
+
+--delete
+delete from employees where emp_no = 9;
+--delete
+
+--update
+update employees set hire_date = '2022-10-09' where emp_no = 1;
+--update
+
+select * from employees;
+
+
 
 
 
