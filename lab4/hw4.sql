ae1797060f47c76bb047bf47a5bcbfd84135fd14
qorganbek <dimashqorganbek@gmail.com>
2022-10-18 21:22:36 +0600

    upd

--- lab4/hw4.sql
+++ lab4/hw4.sql
@@ -1,1 +1,46 @@
-select * from section;
+--ex1
+--a
+select title from course
+where dept_name = 'Biology' and credits > 3;
+
+--b
+select * from classroom
+where building = 'Taylor' or building = 'Watson';
+
+--c
+select * from course
+where dept_name = 'Comp. Sci.';
+
+--d
+select  DISTINCT course.course_id,title,dept_name,credits from course
+left join section on course.course_id = section.course_id
+where semester = 'Spring';
+
+--e
+select * from student
+where tot_cred between 45 and 85;
+
+--f
+select * from course
+where title like '%e' or title like '%a' or title like '%u' or title like '%i' or title like '%o';
+
+-- select * from course
+-- where title like '%[aeiou]';
+-- select * from course
+-- where title like '.*[aeiou]$';
+
+--g
+
+select * from course
+where course_id = 'EE-181';
+
+select prereq_id from course
+join prereq p on course.course_id = p.course_id
+where p.course_id = 'EE-181';
+
+--ex 2
+--a
+select * from instructor
+full join department d on instructor.dept_name = d.dept_name;
+
+
